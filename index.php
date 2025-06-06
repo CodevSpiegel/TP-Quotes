695afa82a7f50026ad5ed421935012a850debd05
Sébastien Spiegel <codev.spiegel@gmail.com>
2025-06-06 17:47:57 +0200

    Transfert

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quotes exercise</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+
+<main>
+<div class="header-main">
+    <div>TP QUOTES</div>
+    <div>
+        <form action="#">
+            <input type="text" placeholder="Rechercher">
+        </form>
+    </div>
+</div>
+<form action="traitement.php" method="POST">
+    <div class="checkbox-block">
+        <div class="form-group">
+            <label for="html">HTML</label>
+            <input type="checkbox" name="html" id="html" />
+        </div>
+        <div class="form-group">
+            <label for="css">CSS</label>
+            <input type="checkbox" name="css" id="css" />
+        </div>
+        <div class="form-group">
+            <label for="js">JS</label>
+            <input type="checkbox" name="js" id="js" />
+        </div>
+        <div class="form-group">
+            <label for="php">PHP</label>
+            <input type="checkbox" name="php" id="php" />
+        </div>
+        <div class="form-group">
+            <label for="sql">SQL</label>
+            <input type="checkbox" name="sql" id="sql" />
+        </div>
+    </div>
+    <div class="submit-block">
+        <div class="button">
+            <button name="submit">Afficher une Quote</button>
+        </div>
+    </div>
+    <div class="text-block">
+        <div class="quote-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum?</div>
+    </div>
+</form>
+
+</main>
+
+</body>
+
+</html>
